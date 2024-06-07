2b9947efc50ef799255f4a42116b9165b78fb0c7
Nellia00 <kornelianeugebauer0@gmail.com>
2024-06-08 00:46:48 +0200

    Folder Models

--- app_with_csv.py
+++ app_with_csv.py
@@ -3,33 +3,33 @@ import pandas as pd
 from datetime import datetime
 import pmdarima as pm
 from sklearn.metrics import mean_squared_error
+import pmdarima as pm
 import os
 import logging
+import pickle
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
 
+# Wczytujemy dane
 file_path = os.path.join('Data', 'weather_data.csv')
-
-try:
-    df = pd.read_csv(file_path)
-except FileNotFoundError:
-    df = None
-    app.logger.debug(f"Plik {file_path} nie został znaleziony.")
-
+df = pd.read_csv(file_path)
 df['date'] = pd.to_datetime(df['date'])
 df.set_index('date', inplace=True)
 df['avgtempC'] = df['avgtempC'].astype(int)
 temp_df = df['avgtempC']
-train_df = temp_df['2018':'2022'].resample('M').mean()
 test_df = temp_df['2023':'2024'].resample('M').mean()
-model = pm.auto_arima(train_df, seasonal=True, m=12)
-model.fit(train_df)
+
+# Wczytujemy wyuczony model
+model_file_path = os.path.join('models', 'weather_forecast_model.pkl')
+with open(model_file_path, 'rb') as model_file:
+    model = pickle.load(model_file)
+app.logger.debug(f"Model wczytany z {model_file_path}")
+
 
 @app.route('/weather_forecast', methods=['GET'])
-def weather_forecast():
-    try:             
+def weather_forecast():          
         forecast = model.predict(n_periods=len(test_df))
         forecast = pd.DataFrame(forecast, index=test_df.index, columns=['Prediction'])
         error = mean_squared_error(test_df, forecast)
@@ -38,13 +38,10 @@ def weather_forecast():
         
         result = {
             'Test Mean Squared Error': error,
-            'Forecast': forecast.to_dict()
+            'Forecast': forecast.to_dict(),
         }
         
         return jsonify(result)
-    
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
